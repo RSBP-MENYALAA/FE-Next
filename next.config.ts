a29413ff8b1f4e683856e5650f76184d19c3a2c6
qiqioberon <aquq1q1.farrukh@gmail.com>
2024-12-16 05:07:37 +0700

    fix: react toastify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true, // Set ke true jika ini redirect permanen (301), false untuk redirect sementara (307)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
